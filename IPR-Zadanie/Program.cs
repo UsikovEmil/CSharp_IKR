268a9354b9bf258909f4088c4b465513a189208b
Emil <usikov.emil@yandex.ru>
2023-02-14 23:26:36 +0500

    Создание проекта 2 (Создано тело)

--- IPR-Zadanie/Program.cs
+++ IPR-Zadanie/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"] 
+// ["1234", "1567", "-2", "computer science"] -> ["-2"] 
+// ["Russia", "Denmark", "Kazan"] -> []
+
+Console.Clear();
+
+string[] array = { "hello", "2", "world", ":-)" };
+
 Console.WriteLine("Hello, World!");
+void massiv(string[] m)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(i);
+    }
+
+}
+
+massiv(array);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(array.Length);
